df01b49e3cc48a88f6313c765037912a2e9190cd
fiorellasaro <fiorella.sfl@gmail.com>
2020-05-14 15:31:09 -0500

    cambio de credenciales

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyDsa4eEQ6WlPIObaddjAa7qAGKRi9NuYKY',
+    key: 'AIzaSyDX1vrBjSyw1iumEpBmvtEZ0gIzQcuu8_I',
     libraries: 'places',
   },
   installComponents: true
@@ -24,14 +24,14 @@ Vue.config.productionTip = false
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB1PtbrxIC7pfT5Vy1R3MPAEyOCjuQa6B8",
-  authDomain: "konectase-522d7.firebaseapp.com",
-  databaseURL: "https://konectase-522d7.firebaseio.com",
-  projectId: "konectase-522d7",
-  storageBucket: "konectase-522d7.appspot.com",
-  messagingSenderId: "1088807972190",
-  appId: "1:1088807972190:web:f8a9f09e09e06a075973cf",
-  measurementId: "G-ZGG15E1TFG"
+  apiKey: "AIzaSyDFb0EaWaPKCq37BuZSkoY2193PNZrUx0I",
+  authDomain: "konecta-seleccion.firebaseapp.com",
+  databaseURL: "https://konecta-seleccion.firebaseio.com",
+  projectId: "konecta-seleccion",
+  storageBucket: "konecta-seleccion.appspot.com",
+  messagingSenderId: "212657214250",
+  appId: "1:212657214250:web:34e59f4f06c30511644453",
+  measurementId: "G-FZ4MPDHR8G"
 };
 firebase.initializeApp(firebaseConfig);
 
